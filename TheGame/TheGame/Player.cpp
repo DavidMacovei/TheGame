f786262865d81e93ca528a5ef67f7ac5dd5e5c57
Bulubenchi Bogdan <bogdan.bulubenchi@student.unitbv.ro>
2025-10-30 19:19:25 +0200

    Updated Player.cpp
    Added definitions for functions and content

--- TheGame/TheGame/Player.cpp
+++ TheGame/TheGame/Player.cpp
@@ -1,1 +1,34 @@
 #include "Player.h"
+
+Player::Player(std::string username)
+{
+	this->username = username;
+}
+
+void Player::addCardToHand(CardDemo c)
+{
+	hand.push_back(c);
+}
+
+CardDemo Player::playCard(int handIndex)
+{
+	CardDemo copy = hand[handIndex];
+	hand.erase(hand.begin() + handIndex);
+	return copy;
+}
+
+int Player::getCardsInHand() const
+{
+	return hand.size();
+}
+
+const std::vector<CardDemo>& Player::getHand() const
+{
+	return hand;
+}
+
+const std::string& Player::getUsername() const
+{
+	return username;
+}
+
