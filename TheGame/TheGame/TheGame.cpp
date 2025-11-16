e6d91c0978f282446d30ac0f97617ac96a7f18e1
Macovei David-Sebastian <macoveidavid.sebastian@gmail.com>
2025-11-16 19:12:49 +0200

    Configured basic HTTP server and added test route /

--- TheGame/TheGame/TheGame.cpp
+++ TheGame/TheGame/TheGame.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <crow.h>
+
+#include "Game.h"
 
 int main()
 {
+	crow::SimpleApp app;
+
+	std::vector<std::string> playerNames = { "Jucator1", "Jucator2", "Jucator3" };
+	Game game(playerNames);
+
+	CROW_ROUTE(app, "/")([](){
+		return "Hello!";
+		});
+
+
+	app.port(18080).multithreaded().run();
+
 	return 0;
 }
