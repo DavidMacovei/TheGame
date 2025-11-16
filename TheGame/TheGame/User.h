897af19dbc17ad06fdd7bbebb6babaf6f7e16030
Eduard Mihaila <edimih24@yahoo.com>
2025-11-16 17:43:01 +0200

    Added User class for DB.

---
+++ TheGame/TheGame/User.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <sstream>
+
+class User {
+public:
+    User();
+    User(const std::string& username, const std::string& pwd_hash);
+
+    int GetId() const;
+    const std::string& GetUsername() const;
+    const std::string& GetPasswordHash() const;
+    int GetScore() const;
+    int GetHoursPlayed() const;
+
+    void SetScore(int s);
+    void SetHoursPlayed(int h);
+    void SetPasswordHash(const std::string& hash);
+
+    // Manual JSON serialization
+    std::string ToJson() const;
+    static User FromJson(const std::string& jsonStr);
+
+private:
+    int m_id;
+    std::string m_username;
+    std::string m_password_hash;
+    int m_score;
+    int m_hours_played;
+};
