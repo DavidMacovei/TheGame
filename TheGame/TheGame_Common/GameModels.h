81c70eacf070519cae753881db5a072baa731183
Macovei David-Sebastian <macoveidavid.sebastian@gmail.com>
2025-12-07 20:41:59 +0200

    Implemented shared data models with JSON serialization (BasicResponse, Auth & Lobby)

--- TheGame/TheGame_Common/GameModels.h
+++ TheGame/TheGame_Common/GameModels.h
@@ -14,19 +14,33 @@ using json = nlohmann::json;
 
 
 struct COMMON_API BasicResponse {
+	std::string status;
+	std::string error;
 
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(BasicResponse, status, error)
 };
 
 struct COMMON_API AuthRequest {
+	std::string username;
+	std::string password;
 
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(AuthRequest, username, password)
 };
 
 struct COMMON_API JoinLobbyRequest {
+	std::string username;
 
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(JoinLobbyRequest, username)
 };
 
 struct COMMON_API LobbyState {
+	std::string status;
+	std::vector<std::string> players;
+	std::vector<std::string> waitingList;
+	int currentPlayers = 0;
+	int neededPlayers = 0;
 
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(LobbyState, status, players, waitingList, currentPlayers, neededPlayers)
 };
 
 struct COMMON_API PlayerState {
