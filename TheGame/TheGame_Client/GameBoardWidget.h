3433656075a2e5c3fd1b527f9e4eef256031b0a0
Ababei Vasile <ababeivasile25@gmail.com>
2025-12-08 00:06:55 +0200

    GameBoardWidget.h
    Updated GameBoardWidget.h

---
+++ TheGame/TheGame_Client/GameBoardWidget.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QWidget>
+#include <QListWidgetItem>
+#include "GameNetworkManager.h"
+
+namespace Ui { class GameBoardWidget; }
+
+class GameBoardWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit GameBoardWidget(const QString& username, QWidget* parent = nullptr);
+    ~GameBoardWidget();
+
+    void start();
+
+private slots:
+    void onGameStateReceived(const QJsonObject& state);
+
+    void onHandItemClicked(QListWidgetItem* item);
+    void onStack1Clicked();
+    void onStack2Clicked();
+    void onStack3Clicked();
+    void onStack4Clicked();
+    void onSendChatClicked();
+    void onEndTurnClicked();
+
+private:
+    Ui::GameBoardWidget* ui;
+    GameNetworkManager* m_net;
+    QString m_username;
+
+    int m_selectedCardIndex = -1;
+
+    void updateStacks(const QJsonArray& stacks);
+    void updateHand(const QJsonArray& players);
+    void updateChat(const QJsonArray& messages);
+    void updateTurnInfo(const QJsonObject& state);
+
+    void handleStackClick(int stackId);
+};
