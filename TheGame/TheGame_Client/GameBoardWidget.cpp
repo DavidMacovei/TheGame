bf9fde3b17603ca2faa23c06dba6052f4003a5ea
Ababei Vasile <ababeivasile25@gmail.com>
2025-12-08 00:10:29 +0200

    GameBoardWidget functions
    GameBoardWidget functions completed

--- TheGame/TheGame_Client/GameBoardWidget.cpp
+++ TheGame/TheGame_Client/GameBoardWidget.cpp
@@ -62,4 +62,104 @@ void GameBoardWidget::updateStacks(const QJsonArray& stacks)
         ui->btnStack3->setText("DESC (100 -> 2)\nTOP: " + getTop(stacks[2], false));
         ui->btnStack4->setText("DESC (100 -> 2)\nTOP: " + getTop(stacks[3], false));
     }
+
+    void GameBoardWidget::updateHand(const QJsonArray & players)
+    {
+        QJsonArray myHand;
+        for (const auto& p : players) {
+            QJsonObject pObj = p.toObject();
+            if (pObj["username"].toString() == m_username) {
+                myHand = pObj["hand"].toArray();
+                break;
+            }
+        }
+
+        if (ui->listHand->count() != myHand.size()) {
+            ui->listHand->clear();
+            for (const auto& c : myHand) {
+                ui->listHand->addItem(QString::number(c.toInt()));
+            }
+            m_selectedCardIndex = -1;
+        }
+    }
+
+    void GameBoardWidget::updateChat(const QJsonArray & messages)
+    {
+        if (ui->listChat->count() != messages.size()) {
+            ui->listChat->clear();
+            for (const auto& m : messages) {
+                QJsonObject mObj = m.toObject();
+                QString line = mObj["username"].toString() + ": " + mObj["text"].toString();
+                ui->listChat->addItem(line);
+            }
+            ui->listChat->scrollToBottom();
+        }
+    }
+
+    void GameBoardWidget::updateTurnInfo(const QJsonObject & state)
+    {
+        int currentIdx = state["currentPlayer"].toInt();
+        QJsonArray players = state["players"].toArray();
+
+        if (currentIdx >= 0 && currentIdx < players.size()) {
+            QString currentPlayer = players[currentIdx].toObject()["username"].toString();
+
+            if (currentPlayer == m_username) {
+                ui->lblStatus->setText("ESTE RANDUL TAU!");
+                ui->lblStatus->setStyleSheet("font-size: 14pt; font-weight: bold; color: green;");
+
+                ui->btnStack1->setEnabled(true);
+                ui->btnStack2->setEnabled(true);
+                ui->btnStack3->setEnabled(true);
+                ui->btnStack4->setEnabled(true);
+                ui->btnEndTurn->setEnabled(true);
+            }
+            else {
+                ui->lblStatus->setText("Asteptam jucatorul: " + currentPlayer);
+                ui->lblStatus->setStyleSheet("font-size: 12pt; color: black;");
+
+                ui->btnStack1->setEnabled(false);
+                ui->btnStack2->setEnabled(false);
+                ui->btnStack3->setEnabled(false);
+                ui->btnStack4->setEnabled(false);
+                ui->btnEndTurn->setEnabled(false);
+            }
+        }
+    }
+
+    void GameBoardWidget::onHandItemClicked(QListWidgetItem * item)
+    {
+        m_selectedCardIndex = ui->listHand->row(item);
+    }
+
+    void GameBoardWidget::handleStackClick(int stackId)
+    {
+        if (m_selectedCardIndex == -1) {
+            QMessageBox::information(this, "Info", "Selecteaza o carte din mana mai intai!");
+            return;
+        }
+
+        m_net->sendMove(m_username, m_selectedCardIndex, stackId);
+
+    }
+
+    void GameBoardWidget::onStack1Clicked() { handleStackClick(0); }
+    void GameBoardWidget::onStack2Clicked() { handleStackClick(1); }
+    void GameBoardWidget::onStack3Clicked() { handleStackClick(2); }
+    void GameBoardWidget::onStack4Clicked() { handleStackClick(3); }
+
+    void GameBoardWidget::onSendChatClicked()
+    {
+        QString msg = ui->lineChatMsg->text().trimmed();
+        if (!msg.isEmpty()) {
+            m_net->sendChatMessage(m_username, msg);
+            ui->lineChatMsg->clear();
+        }
+    }
+
+    void GameBoardWidget::onEndTurnClicked()
+    {
+        m_net->endTurn(m_username);
+    }
+
 }
