5bef4aff6f0a93fbb76aaaa825fd9d97f8f7764b
Bulubenchi Bogdan <bogdan.bulubenchi@student.unitbv.ro>
2025-12-31 12:03:50 +0200

    StackWidget.h

--- TheGame/TheGame_Client/StackWidget.h
+++ TheGame/TheGame_Client/StackWidget.h
@@ -1,5 +1,24 @@
 #pragma once
-class StackWidget
-{
-};
+#include <QWidget>
+#include <vector>
+#include "CardWidget.h"
 
+class StackWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    enum class StackType { Ascending, Descending };
+    explicit StackWidget(StackType type, QWidget* parent = nullptr);
+
+    void setCards(const std::vector<uint8_t>& cards);
+    StackType stackType() const;
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    StackType m_type;
+    std::vector<CardWidget*> m_cardWidgets;
+    std::vector<uint8_t> m_cards;
+    void updateStack();
+};
