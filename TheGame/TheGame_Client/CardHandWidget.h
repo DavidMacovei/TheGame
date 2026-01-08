ab4f2f15ccf0268d5b7f04fd5dabbdfb60b2dcaa
Bulubenchi Bogdan <bogdan.bulubenchi@student.unitbv.ro>
2026-01-08 14:41:54 +0200

    Update CardHandWidget.h

--- TheGame/TheGame_Client/CardHandWidget.h
+++ TheGame/TheGame_Client/CardHandWidget.h
@@ -1,5 +1,22 @@
 #pragma once
-class CardHandWidget
-{
-};
+#include <QWidget>
+#include <vector>
+#include "CardWidget.h"
 
+class CardHandWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CardHandWidget(QWidget* parent = nullptr);
+
+    void setCards(const std::vector<uint8_t>& cards);
+    int cardCount() const;
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    std::vector<CardWidget*> m_cardWidgets;
+    std::vector<uint8_t> m_cards;
+    void updateHand();
+};
