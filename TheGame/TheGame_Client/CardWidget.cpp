5b7a837b61093c1b4544e4c22c3e721e97302114
Ababei Vasile <ababeivasile25@gmail.com>
2026-01-09 10:46:17 +0200

    Updated CardWidget
    Updated CardWidget

--- TheGame/TheGame_Client/CardWidget.cpp
+++ TheGame/TheGame_Client/CardWidget.cpp
@@ -1,4 +1,4 @@
-#include "CardWidget.h"
+﻿#include "CardWidget.h"
 #include <QPainter>
 
 CardWidget::CardWidget(uint8_t value, QWidget* parent)
@@ -23,13 +23,22 @@ void CardWidget::paintEvent(QPaintEvent*)
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
-    // Draw card rectangle
-    painter.setBrush(Qt::white);
-    painter.setPen(Qt::black);
-    painter.drawRect(rect().adjusted(2, 2, -2, -2));
+    if (m_value == 0) {
+        painter.setBrush(QColor("#1a237e"));
+        painter.setPen(QPen(QColor("white"), 2));
+        painter.drawRoundedRect(rect().adjusted(2, 2, -2, -2), 5, 5);
 
-    // Draw value centered
-    painter.setPen(Qt::black);
-    painter.setFont(QFont("Arial", 18, QFont::Bold));
-    painter.drawText(rect(), Qt::AlignCenter, QString::number(m_value));
+        painter.setPen(QColor("#5c6bc0"));
+        painter.setFont(QFont("Arial", 24));
+        painter.drawText(rect(), Qt::AlignCenter, "🂠");
+    }
+    else {
+        painter.setBrush(Qt::white);
+        painter.setPen(QPen(Qt::black, 2));
+        painter.drawRoundedRect(rect().adjusted(2, 2, -2, -2), 5, 5);
+
+        painter.setPen(Qt::black);
+        painter.setFont(QFont("Arial", 18, QFont::Bold));
+        painter.drawText(rect(), Qt::AlignCenter, QString::number(m_value));
+    }
 }
