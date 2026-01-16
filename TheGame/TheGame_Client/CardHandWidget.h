2d85fa03966befbfb81bfb6bd492962f9c4e82a1
Ababei Vasile <ababeivasile25@gmail.com>
2026-01-16 13:37:33 +0200

    CardHandWidget
    Updated CardHandWidget for orientation

--- TheGame/TheGame_Client/CardHandWidget.h
+++ TheGame/TheGame_Client/CardHandWidget.h
@@ -7,10 +7,16 @@ class CardHandWidget : public QWidget
 {
     Q_OBJECT
 public:
+    enum class Orientation {
+        Horizontal,  // For bottom/top players
+        Vertical     // For left/right players
+    };
+
     explicit CardHandWidget(QWidget* parent = nullptr);
 
     void setCards(const std::vector<uint8_t>& cards);
     int cardCount() const;
+    void setOrientation(Orientation orientation);
 
 signals:
     void cardClicked(int cardIndex);
@@ -23,5 +29,6 @@ private:
     std::vector<CardWidget*> m_cardWidgets;
     std::vector<uint8_t> m_cards;
     int m_selectedIndex = -1;
+    Orientation m_orientation = Orientation::Horizontal;
     void updateHand();
 };
